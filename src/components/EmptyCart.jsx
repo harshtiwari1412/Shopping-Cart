9ba06651f300433d34bdb2722126f7bfd5099451
Harsh Tiwari <harshtiwari1412@gmail.com>
2025-07-25 22:42:48 +0530

    Empty Cart

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import "./EmptyCart.css"
 
 function EmptyCart() {
 
@@ -9,7 +10,7 @@ function EmptyCart() {
     }
     
   return (
-    <div>
+    <div className='empty-cart'>
         <h1>Your Cart is Empty</h1>
         <button onClick={homeHandler}>Shop Now</button>
     </div>
